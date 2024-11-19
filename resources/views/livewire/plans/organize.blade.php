3fcafa6728fa3453d7ba01d64bf175418642718e
Cocoa <felmenyt123@gmail.com>
2024-11-19 02:21:52 -0500

    no se

--- resources/views/livewire/plans/organize.blade.php
+++ resources/views/livewire/plans/organize.blade.php
@@ -1,7 +1,22 @@
 <div>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         @foreach ($plansDefault as $default)
-            <a href="{{ route('planBuilding', $default->id) }}" wire:click="prueba({{ $default->id }})"
+            @auth
+                <a href="{{ route('planBuilding', $default->id) }}" wire:click="prueba({{ $default->id }})"
+                    class="w-full max-w-sm h-auto rounded-lg shadow-lg overflow-hidden group block">
+                    <div class="overflow-hidden">
+                        <img src="{{ asset($default->url_img) }}"
+                            class="w-full h-80 object-cover rounded-lg transform transition duration-500 group-hover:scale-110">
+                    </div>
+                    <div class="p-4">
+                        <h3 class="text-2xl md:text-3xl pb-3 font-bold">{{ $default->name }}</h3>
+                        <div class="flex justify-center md:justify-evenly">
+                            <p class="text-gray-500 text-lg md:text-xl">Ver plan completo</p>
+                        </div>
+                    </div>
+                </a>
+            @else
+            <a href="{{ route('login') }}"
                 class="w-full max-w-sm h-auto rounded-lg shadow-lg overflow-hidden group block">
                 <div class="overflow-hidden">
                     <img src="{{ asset($default->url_img) }}"
@@ -14,6 +29,7 @@
                     </div>
                 </div>
             </a>
+            @endauth
         @endforeach
         <a href="{{ route('planBuilding', $planCount) }}" wire:click="prueba({{ $planCount }})"
             class="w-full max-w-sm h-auto rounded-lg shadow-lg overflow-hidden group block">
@@ -31,7 +47,7 @@
 {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
 
 {{-- <div class="overflow-hidden">
-        <img src="{{ asset($default->url_img)}}"  
+        <img src="{{ asset($default->url_img)}}"
             class="w-full h-80 object-cover rounded-lg transform transition duration-500 group-hover:scale-110">
     </div>
     <div class="p-4">
--- resources/views/livewire/service/all-activities.blade.php
+++ resources/views/livewire/service/all-activities.blade.php
@@ -2,35 +2,67 @@
     <div class="p-4 ml-1">
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             @foreach ($activities as $activity)
-                <a href="{{ route('activityInfo', $activity->id)}}">
-                    <div class="w-full border rounded-lg shadow-lg relative group">
-                        <div class="card shadow-md w-full h-[20rem] group gap-[1.3em] rounded-sm relative flex justify-end flex-col z-[1] overflow-hidden">
-                            <div class="absolute top-0 left-0 h-full w-full">
-                                <div class="w-full h-full transition-transform duration-1000 transform group-hover:scale-110" wire:key="site-{{ $activity->id }}">
-                                    <img class="w-full h-full object-cover" src="{{ asset($activity->url_img) }}?v={{ now()->timestamp }}" loading="lazy">
-                                </div>
+                @auth
+                    <a href="{{ route('activityInfo', $activity->id)}}">
+                        <div class="w-full border rounded-lg shadow-lg relative group">
+                            <div class="card shadow-md w-full h-[20rem] group gap-[1.3em] rounded-sm relative flex justify-end flex-col z-[1] overflow-hidden">
+                                <div class="absolute top-0 left-0 h-full w-full">
+                                    <div class="w-full h-full transition-transform duration-1000 transform group-hover:scale-110" wire:key="site-{{ $activity->id }}">
+                                        <img class="w-full h-full object-cover" src="{{ asset($activity->url_img) }}?v={{ now()->timestamp }}" loading="lazy">
+                                    </div>
 
-                            </div>
-                            <div class="container bg-black bg-opacity-35 hover:bg-opacity-85 z-[2] relative font-nunito flex flex-col p-[1.5rem] gap-[0.5rem]">
-                                <div class="h-fit w-full">
-                                    <h1 class="text-[1.3rem] font-semibold text-white">
-                                        {{ $activity->title }}
-                                    </h1>
                                 </div>
-                                <p class="font-nunito block text-blue-50 font-light relative h-[0em] group-hover:h-[6em] leading-[1.2em] duration-500 overflow-hidden">
-                                    {{ $activity->information_below }}
-                                </p>
-                                <div class="flex items-center">
-                                    @for ($i = 1; $i <= 5; $i++)
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 cursor-pointer {{ $i <= $activity->rating ? 'text-yellow-500' : 'text-gray-400' }}" fill="{{ $i <= $activity->rating ? 'currentColor' : 'none' }}" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354l2.847 5.751 6.351.922-4.6 4.48 1.085 6.327L12 18.6l-5.683 3.017 1.085-6.327-4.6-4.48 6.351-.922L12 4.354z" />
-                                        </svg>
-                                    @endfor
+                                <div class="container bg-black bg-opacity-35 hover:bg-opacity-85 z-[2] relative font-nunito flex flex-col p-[1.5rem] gap-[0.5rem]">
+                                    <div class="h-fit w-full">
+                                        <h1 class="text-[1.3rem] font-semibold text-white">
+                                            {{ $activity->title }}
+                                        </h1>
+                                    </div>
+                                    <p class="font-nunito block text-blue-50 font-light relative h-[0em] group-hover:h-[6em] leading-[1.2em] duration-500 overflow-hidden">
+                                        {{ $activity->information_below }}
+                                    </p>
+                                    <div class="flex items-center">
+                                        @for ($i = 1; $i <= 5; $i++)
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 cursor-pointer {{ $i <= $activity->rating ? 'text-yellow-500' : 'text-gray-400' }}" fill="{{ $i <= $activity->rating ? 'currentColor' : 'none' }}" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354l2.847 5.751 6.351.922-4.6 4.48 1.085 6.327L12 18.6l-5.683 3.017 1.085-6.327-4.6-4.48 6.351-.922L12 4.354z" />
+                                            </svg>
+                                        @endfor
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </a>
+                    </a>
+                @else
+                    <a href="{{ route('login')}}">
+                        <div class="w-full border rounded-lg shadow-lg relative group">
+                            <div class="card shadow-md w-full h-[20rem] group gap-[1.3em] rounded-sm relative flex justify-end flex-col z-[1] overflow-hidden">
+                                <div class="absolute top-0 left-0 h-full w-full">
+                                    <div class="w-full h-full transition-transform duration-1000 transform group-hover:scale-110" wire:key="site-{{ $activity->id }}">
+                                        <img class="w-full h-full object-cover" src="{{ asset($activity->url_img) }}?v={{ now()->timestamp }}" loading="lazy">
+                                    </div>
+
+                                </div>
+                                <div class="container bg-black bg-opacity-35 hover:bg-opacity-85 z-[2] relative font-nunito flex flex-col p-[1.5rem] gap-[0.5rem]">
+                                    <div class="h-fit w-full">
+                                        <h1 class="text-[1.3rem] font-semibold text-white">
+                                            {{ $activity->title }}
+                                        </h1>
+                                    </div>
+                                    <p class="font-nunito block text-blue-50 font-light relative h-[0em] group-hover:h-[6em] leading-[1.2em] duration-500 overflow-hidden">
+                                        {{ $activity->information_below }}
+                                    </p>
+                                    <div class="flex items-center">
+                                        @for ($i = 1; $i <= 5; $i++)
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 cursor-pointer {{ $i <= $activity->rating ? 'text-yellow-500' : 'text-gray-400' }}" fill="{{ $i <= $activity->rating ? 'currentColor' : 'none' }}" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354l2.847 5.751 6.351.922-4.6 4.48 1.085 6.327L12 18.6l-5.683 3.017 1.085-6.327-4.6-4.48 6.351-.922L12 4.354z" />
+                                            </svg>
+                                        @endfor
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                @endauth
             @endforeach
         </div>
     </div>
